95c5e53fd51e6d62fdfd2f5372686d540fb9566a
bolaji87 <sakabolaji77@gmail.com>
2025-01-09 01:31:06 +0100

    feat: add login to navbar component

---
+++ src/components/Login.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { RxHamburgerMenu } from "react-icons/rx";
+
+const Login = () => {
+  return (
+    <div className="login">
+      <button className="btn-login"> Login</button>
+      <RxHamburgerMenu className="hamburger-menu" />
+    </div>
+  );
+};
+
+export default Login;
