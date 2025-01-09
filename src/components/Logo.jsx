822cbcf6f94793cd1eb9898b292884787c3f0a2e
bolaji87 <sakabolaji77@gmail.com>
2025-01-09 01:30:32 +0100

    feat; add logo component to navbar component

---
+++ src/components/Logo.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { DiApple } from "react-icons/di";
+import { Md10K } from "react-icons/md";
+
+import "./stylesComponents.css";
+
+const Logo = () => {
+  return (
+    <div id="logo">
+      <DiApple />
+      <Md10K />
+
+      <div>Modish Dynamics</div>
+    </div>
+  );
+};
+
+export default Logo;
