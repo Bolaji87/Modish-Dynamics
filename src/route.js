0fa7c0c8e5dda5942a4e9380b9076bf2e3374537
bolaji87 <sakabolaji77@gmail.com>
2025-01-10 11:55:16 +0100

    feat: add Product page to route

--- src/route.js
+++ src/route.js
@@ -1,8 +1,16 @@
-import HomePage from "../src/pages/HomePage.jsx";
+import { lazy } from "react";
+import React from "react";
+
+import HomePage from "../src/pages/home/HomePage.jsx";
+const Products = lazy(() => import("./pages/products/Products.jsx"));
 
 export const routes = [
   {
     path: "/",
     element: HomePage,
+  },
+  {
+    path: "/products",
+    element: Products,
   },
 ];
