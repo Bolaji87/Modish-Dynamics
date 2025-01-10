7dbf49a5e48ae20c072ee67be73de3c17d958fbf
bolaji87 <sakabolaji77@gmail.com>
2025-01-10 12:37:54 +0100

    feat : remove navbar from homepage

---
+++ src/pages/home/HomePage.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+import "./home.css";
+import Hero from "../../components/hero/Hero";
+
+Hero;
+const HomePage = () => {
+  return (
+    <div className="home-page">
+      <Hero />
+    </div>
+  );
+};
+
+export default HomePage;
