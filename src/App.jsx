914435c37993a16a78d8e6e21e0d1ec2327787ba
bolaji87 <sakabolaji77@gmail.com>
2025-01-15 12:27:02 +0100

    feat: add Footer component

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import { routes } from "./route";
 import Navbar from "./components/navbar/Navbar";
+import Footer from "./components/footer/Footer";
 const App = () => {
   return (
     <BrowserRouter>
@@ -17,6 +18,7 @@ const App = () => {
             />
           ))}
         </Routes>
+        <Footer />
       </Suspense>
     </BrowserRouter>
   );
