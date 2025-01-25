2d7bdc62e4f17d654eb2c9c429f093cb0917e45c
bolaji87 <sakabolaji77@gmail.com>
2025-01-25 01:38:48 +0100

    feat:use footer

--- src/pages/home/HomePage.jsx
+++ src/pages/home/HomePage.jsx
@@ -2,12 +2,16 @@ import React from "react";
 
 import "./home.css";
 import Hero from "../../components/hero/Hero";
+import Newsletter from "../../components/newsletter/Newsletter";
+import Footer from "../../components/footer/Footer";
 
 Hero;
 const HomePage = () => {
   return (
     <div className="home-page">
       <Hero />
+      <Newsletter />
+      <Footer />
     </div>
   );
 };
