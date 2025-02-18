e3399ea9121044ad6bccb478eb742fa9aed93617
Bolaji87 <sakabolaji77@gmail.com>
2025-02-18 14:44:28 +0100

    feat:use routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,24 @@
 import React from "react";
 import Navbar from "./components/mainheader/Navbar";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { appRoute } from "./UI/routes";
 
 function App() {
   return (
     <div className="app">
       <Navbar />
+      <BrowserRouter>
+        <Routes>
+          {appRoute.map((route) => (
+            <Route
+              exact
+              key={route.path}
+              path={route.path}
+              element={<route.component />}
+            />
+          ))}
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
