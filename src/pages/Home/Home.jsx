5298346a5d26f0f1c16138ac03d51b483b7bb71c
Bolaji87 <sakabolaji77@gmail.com>
2025-02-19 13:37:12 +0100

    feat: call Hero component in Home

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import styles from "./Home.module.css";
+import Hero from "../../components/Hero/Hero";
 
 function Home() {
   return (
     <div>
-      <h1>Welcome to Modish Dynamics</h1>
+      <Hero />
     </div>
   );
 }
