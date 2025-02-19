8cd88e2a635473bdfe33dfe662f3043942857f4e
Bolaji87 <sakabolaji77@gmail.com>
2025-02-19 13:36:43 +0100

    feat: build Hero component and css

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,7 +1,23 @@
 import React from "react";
+import styles from "./Hero.module.css";
+import { Link } from "react-router-dom";
 
 function Hero() {
-  return <div>Hero</div>;
+  return (
+    <section className={styles["hero-cont"]}>
+      <div>
+        <p className={styles.content}>
+          Welcome to Modish Dynamics, welcome to <br />
+          our wall of modest fashion where style meets sophistication.
+        </p>
+
+        <h1>Modish Dynamics</h1>
+        <button className={styles["btn-shop-now"]}>
+          <Link to="/products">shop now</Link>
+        </button>
+      </div>
+    </section>
+  );
 }
 
 export default Hero;
