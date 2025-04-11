9746a2ed3a06fef6cca65d5ab04260c702365e4d
Bolaji87 <sakabolaji77@gmail.com>
2025-04-11 11:16:43 +0100

    feat: accept the products to display individual product

--- src/features/products/ProductCard.jsx
+++ src/features/products/ProductCard.jsx
@@ -1,7 +1,20 @@
 import React from "react";
 
-function ProductCard() {
-  return <div>ProductCard</div>;
+function ProductCard({ product }) {
+  const { title, image, price, category, id } = product;
+
+  return (
+    <li className="shadow-custom flex w-80 max-w-[800px] flex-col items-center gap-1 rounded-xl bg-white p-6 transition-shadow duration-300 hover:shadow-lg">
+      <img src={image} alt={title} className="h-72 w-64 object-fill" />
+      <div>
+        <p>{title}</p>
+        <div className="flex">
+          <p>{price}</p>
+          <button>Add to cart</button>
+        </div>
+      </div>
+    </li>
+  );
 }
 
 export default ProductCard;
