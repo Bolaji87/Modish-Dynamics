7777ce6d579a4846f8fe53565f921314a35af009
Bolaji87 <sakabolaji77@gmail.com>
2025-04-11 11:15:12 +0100

    feat: add error if there is an error

--- src/UI/Error.jsx
+++ src/UI/Error.jsx
@@ -1,8 +1,17 @@
 /* eslint-disable react/prop-types */
 import React from "react";
+import { useNavigate, useRouteError } from "react-router-dom";
 
-function Error({ errorMessage }) {
-  return <div>{errorMessage}</div>;
+function Error() {
+  const error = useRouteError();
+  const navigate = useNavigate();
+  return (
+    <div className="mt-20 space-y-2 text-center">
+      <h2 className="mb-4 text-2xl font-bold">Something went wrong 😱</h2>
+      <p className="font-semibold">{error.data || error.message}</p>
+      <button onClick={() => navigate(-1)}>Go back</button>
+    </div>
+  );
 }
 
 export default Error;
