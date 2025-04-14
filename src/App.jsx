ad4ed7fe6a4119a4f0233483455e4648e6fd7d95
Bolaji87 <sakabolaji77@gmail.com>
2025-04-14 19:41:28 +0100

    feat:changes

--- src/App.jsx
+++ src/App.jsx
@@ -3,24 +3,32 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 import AppLayout from "./UI/AppLayout";
 import Home from "./features/main/Home";
-import Products from "./features/products/Products";
+import Error from "./UI/Error";
+import Products, {
+  loader as productsLoader,
+} from "./features/products/Products";
 import Cart from "./features/cart/Cart";
 
 const router = createBrowserRouter([
   {
     element: <AppLayout />,
+    errorElement: <Error />,
     children: [
       {
         element: <Home />,
         path: "/",
+        errorElement: <Error />,
       },
       {
         element: <Products />,
         path: "/products",
+        loader: productsLoader,
+        errorElement: <Error />,
       },
       {
         element: <Cart />,
         path: "/cart",
+        errorElement: <Error />,
       },
     ],
   },
