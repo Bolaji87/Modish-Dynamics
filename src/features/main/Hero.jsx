532dd90c703d14f4e395cecc90f59809427054c2
Bolaji87 <sakabolaji77@gmail.com>
2025-04-14 19:40:35 +0100

    feat: changes image and bg styles

--- src/features/main/Hero.jsx
+++ src/features/main/Hero.jsx
@@ -1,19 +1,24 @@
 import React from "react";
-import { Link } from "react-router-dom";
 
 function Hero() {
   return (
-    <section>
-      <div>
-        <p>
+    <section className="flex h-full items-center gap-6 px-5 py-4 sm:gap-28">
+      <div className="sm:mt-15 space-y-6 pl-10 sm:pt-12 sm:text-xl sm:font-medium">
+        <p className="">
           Welcome to Modish Dynamics, welcome to <br />
           our wall of modest fashion where style meets sophistication.
         </p>
 
-        <h1>Modish Dynamics</h1>
-        <button>
-          <Link to="/products">shop now</Link>
+        <h1 className="mb-10">Modish Dynamics</h1>
+        <button className="border bg-orange-300 sm:inline-block">
+          shop now
         </button>
+      </div>
+      <div className="h-400px">
+        <img
+          src="./bundle-yard-here.webp"
+          className="h-[300px] w-[450px] object-contain sm:h-[300px] sm:object-cover md:object-cover"
+        />
       </div>
     </section>
   );
