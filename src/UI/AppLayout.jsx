5711e8765eb5a8eb0589faef41b199aa266448c0
Bolaji87 <sakabolaji77@gmail.com>
2025-04-14 21:08:56 +0100

    feat:call Footer component

--- src/UI/AppLayout.jsx
+++ src/UI/AppLayout.jsx
@@ -2,18 +2,20 @@ import React from "react";
 import Navbar from "./Navbar";
 import { Outlet, useNavigation } from "react-router-dom";
 import Loader from "./Loader";
+import Footer from "./Footer";
 
 function AppLayout() {
   const navigation = useNavigation();
   const isLoading = navigation.state === "loading";
 
   return (
-    <div className="mx-6 h-screen px-6">
+    <div className="h-screen">
       {isLoading && <Loader />}
       <Navbar />
-      <main className="bg-slate-200">
+      <main className="mx-4 px-4">
         <Outlet />
       </main>
+      <Footer />
     </div>
   );
 }
