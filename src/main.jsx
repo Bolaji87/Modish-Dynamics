9b36af22f20cdd526e2ca0f57d70a6a07397ccb2
Bolaji87 <sakabolaji77@gmail.com>
2025-04-15 15:33:04 +0100

    feat:use redux Provider component and pass in store from store.js

--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,13 @@ import { createRoot } from "react-dom/client";
 
 import "./index.css";
 import App from "./App.jsx";
-import { CartProvider } from "./contexts/CartContext.jsx";
+import { Provider } from "react-redux";
+import store from "../store.js";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <CartProvider>
+    <Provider store={store}>
       <App />
-    </CartProvider>
+    </Provider>
   </StrictMode>,
 );
