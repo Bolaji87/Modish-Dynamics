fd38467c03de1b801108415a6a43d41770ed7809
Bolaji87 <sakabolaji77@gmail.com>
2025-04-15 22:21:30 +0100

    feat: create updateQuantity component

---
+++ src/features/cart/UpdateCartQuantity.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function UpdateCartQuantity() {
+  return (
+    <div className="ml-3 flex gap-3">
+      <button>-</button>
+      <p>2</p>
+      <button>+</button>
+    </div>
+  );
+}
+
+export default UpdateCartQuantity;
