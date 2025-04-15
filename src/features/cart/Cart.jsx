0f14b49f7caca79c5248f0aed841eb1deaa28cf4
Bolaji87 <sakabolaji77@gmail.com>
2025-04-15 15:03:46 +0100

    feat: create dummy cart array and map to simulate real data

--- src/features/cart/Cart.jsx
+++ src/features/cart/Cart.jsx
@@ -1,7 +1,33 @@
 import React from "react";
+import CartItem from "./CartItem";
+
+// const cart = [{ title, image, price, category, id }];
+const cart = [
+  {
+    id: 1,
+    title: "Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops",
+    price: 109.95,
+    image: "https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg",
+  },
+  {
+    id: 2,
+    title: "Mens Casual Premium Slim Fit T-Shirts ",
+    price: 22.3,
+    image:
+      "https://fakestoreapi.com/img/71-3HjGNDUL._AC_SY879._SX._UX._SY._UY_.jpg",
+  },
+];
 
 function Cart() {
-  return <div>Cart</div>;
+  return (
+    <div>
+      <ul>
+        {cart.map((item) => (
+          <CartItem item={item} key={item.id} />
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 export default Cart;
