bf37e5a729c091f5094e2d9e903921c8d633aac9
Bolaji87 <sakabolaji77@gmail.com>
2025-06-12 17:28:07 +0100

    add confirm alert when deleting a cart

--- src/features/cart/UpdateCartQuantity.jsx
+++ src/features/cart/UpdateCartQuantity.jsx
@@ -9,6 +9,16 @@ import {
 import { HiTrash } from "react-icons/hi2";
 function UpdateCartQuantity({ id, quantity }) {
   const dispatch = useDispatch();
+
+  const handleDelete = () => {
+    const confirmDelete = window.confirm(
+      "Are you sure you want to delete this?",
+    );
+    if (confirmDelete) {
+      dispatch(deleteCartItem(id));
+    }
+  };
+
   return (
     <div className="flex items-center gap-2">
       <Button
@@ -26,7 +36,7 @@ function UpdateCartQuantity({ id, quantity }) {
       </Button>
 
       <Button
-        onclick={() => dispatch(deleteCartItem(id))}
+        onclick={handleDelete}
         className="ml-28 inline-flex items-center justify-center rounded-md bg-red-50 p-2 text-red-600 transition hover:bg-red-100 hover:text-red-700 focus:outline-none focus:ring-2 focus:ring-red-400 focus:ring-offset-2"
       >
         <HiTrash className="text-2xl" />
